5cd28a702099c83ec545af016655a84c4096dd3b
zeleke <zzdd9251@gmail.com>
2020-04-23 21:07:26 -0700

    add initial..

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React,{  Component} from "react";
+import ReactDom from "react-dom"
+import "./assets/style.css"
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
+import result from "./components/result"
+import Result from "./components/result";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class QuizBee extends Component{
+    state = {
+        questionBank: [],
+        score:0,
+        responses:0
+    }
+    getQuestions = () => {
+        quizService().then(question => {
+        this.setState({
+            questionBank:question
+        })
+    })
+}
+
+
+computeAnswer = (answer,correctAnswer) => {
+    if (answer === correctAnswer){
+        this.setState({
+            score:this.state.score + 1
+        })
+
+    }
+    this.setState({
+        responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+    })
+};
+
+playAgain = () => {
+    this.getQuestions();
+    this.setState({
+        score:0,
+        responses:0
+    })
+
+}
+componentDidMount() {
+    this.getQuestions()
+}
+
+    render(){
+        return(
+        <div className="container">
+            <div className="title">QuizBee</div>
+            {this.state.questionBank.length > 0 &&
+              this.state.responses < 5 &&
+               this.state.questionBank.map(
+                   ({question,answers,correct,questionID}) => (
+               <QuestionBox 
+               question={question}
+               options={answers}
+               key={questionID}
+               selected={answer => this.computeAnswer(answer,correct)}
+               />
+               )
+               )}
+        {this.state.responses === 5 ? (
+        <Result score={this.state.score} playAgain={this.playAgain} />): null}
+        </div>
+        )
+    }
+        
+}
+
+ReactDom.render(<QuizBee />, document.getElementById("root"));
